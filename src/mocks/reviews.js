476169863da6387f5f28671476fa207361a00d59
ilyazverev1988 <ilya.verez@mail.ru>
2020-10-17 12:47:51 +0300

    add tabs + more-like-films

--- src/mocks/reviews.js
+++ src/mocks/reviews.js
@@ -3,21 +3,24 @@ export default [
     film: `Aviator`,
     review: [
       {
-        textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
+        id: 1,
+        textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere1.`,
         filmScore: `1`,
-        nameUser: `Ubi est albus ionicis tormento?`,
+        nameUser: `Ubi est albus1?`,
         dateReview: `December 24, 2018`
       },
       {
-        textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
+        id: 2,
+        textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere2.`,
         filmScore: `2`,
-        nameUser: `Ubi est albus ionicis tormento?`,
+        nameUser: `Ubi est albus2?`,
         dateReview: `December 20, 2018`
       },
       {
-        textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
+        id: 3,
+        textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere3.`,
         filmScore: `3`,
-        nameUser: `Ubi est albus ionicis tormento?`,
+        nameUser: `Ubi est albus3?`,
         dateReview: `December 21, 2018`
       }
     ]
@@ -26,12 +29,14 @@ export default [
     film: `Dardjeeling limited`,
     review: [
       {
+        id: 1,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `4`,
         nameUser: `Ubi est albus ionicis tormento?`,
         dateReview: `December 10, 2018`
       },
       {
+        id: 2,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `5`,
         nameUser: `Ubi est albus ionicis tormento?`,
@@ -43,12 +48,14 @@ export default [
     film: `Bohemian rhapsody`,
     review: [
       {
+        id: 2,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `3`,
         nameUser: `Ubi est albus ionicis tormento?`,
         dateReview: `December 13, 2018`
       },
       {
+        id: 3,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `1`,
         nameUser: `Ubi est albus ionicis tormento?`,
@@ -60,18 +67,21 @@ export default [
     film: `Fantastic beasts the crimes of grindelwald`,
     review: [
       {
+        id: 1,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `3`,
         nameUser: `Ubi est albus ionicis tormento?`,
         dateReview: `December 10, 2010`
       },
       {
+        id: 2,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `5`,
         nameUser: `Ubi est albus ionicis tormento?`,
         dateReview: `December 10, 2010`
       },
       {
+        id: 3,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `4`,
         nameUser: `Ubi est albus ionicis tormento?`,
@@ -83,12 +93,14 @@ export default [
     film: `Johnny english`,
     review: [
       {
+        id: 1,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `2`,
         nameUser: `Ubi est albus ionicis tormento?`,
         dateReview: `December 12, 2010`
       },
       {
+        id: 2,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `3`,
         nameUser: `Ubi est albus ionicis tormento?`,
@@ -100,6 +112,7 @@ export default [
     film: `Macbeth`,
     review: [
       {
+        id: 1,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `5`,
         nameUser: `Ubi est albus ionicis tormento?`,
@@ -111,12 +124,14 @@ export default [
     film: `No country for old men`,
     review: [
       {
+        id: 2,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `5`,
         nameUser: `Ubi est albus ionicis tormento?`,
         dateReview: `December 6, 2010`
       },
       {
+        id: 3,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `5`,
         nameUser: `Ubi est albus ionicis tormento?`,
@@ -128,6 +143,7 @@ export default [
     film: `War of the worlds`,
     review: [
       {
+        id: 1,
         textReview: `Courage is not one in shangri-la, the country of result, or earth, but everywhere.`,
         filmScore: `5`,
         nameUser: `Ubi est albus ionicis tormento?`,
